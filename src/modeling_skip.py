cf33c72c2a87e47b1dd779e006a5a071024bfc9a
Kira Selby <kaselby@uwaterloo.ca>
2025-06-18 13:29:26 -0400

    Add missing lines
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>

--- src/modeling_skip.py
+++ src/modeling_skip.py
@@ -415,6 +415,24 @@ def build_skip_connection_model_for_causal_lm(pretrained_model_class: type[PreTr
 
             # Initialize weights and apply final processing
             self.post_init()
+            
+        def get_input_embeddings(self):
+            return self.model.embed_tokens
+
+        def set_input_embeddings(self, value):
+            self.model.embed_tokens = value
+
+        def get_output_embeddings(self):
+            return self.lm_head
+
+        def set_output_embeddings(self, new_embeddings):
+            self.lm_head = new_embeddings
+
+        def set_decoder(self, decoder):
+            self.model = decoder
+
+        def get_decoder(self):
+            return self.model
 
         def get_predictor_parameters(self):
             """Get parameters of all predictor networks for optimization."""
