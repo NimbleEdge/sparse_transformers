7fc87c322b86d7c01c902691b2ca9ef84e03fcf5
Kira Selby <kaselby@uwaterloo.ca>
2025-07-07 11:22:45 -0400

    Updated activation capture to work with refactor
    Signed-off-by: Kira Selby <kaselby@uwaterloo.ca>

--- src/models/gemma3n/__init__.py
+++ src/models/gemma3n/__init__.py
@@ -7,7 +7,4 @@ from .modelling_gemma_skip import Gemma3nSkipConnectionForCausalLM
 AutoConfig.register("gemma3n-skip", Gemma3nSkipConnectionConfig)
 AutoModelForCausalLM.register(Gemma3nSkipConnectionConfig, Gemma3nSkipConnectionForCausalLM)
 
-from src.activation_capture import register_activation_capture, ActivationCaptureDefault
-register_activation_capture('gemma3n', ActivationCaptureDefault)
-
 __all__ = [configuration_gemma_skip, modelling_gemma_skip]
